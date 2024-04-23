8ee0d563e30456e3e9acd8cd0fee305306eaf6e5
Tanzeel Ur Rehman <tanzeel498@gmail.com>
2024-04-23 12:55:51 +0500

    add top posts to homePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import TopicCreateForm from '@/components/topics/topic-create-form';
 import TopicsList from '@/components/topics/topics-list';
+import PostList from '@/components/posts/post-list';
 import { Divider } from '@nextui-org/divider';
+import { fetchTopPosts } from '@/db/queries/posts';
 
 export default function Home() {
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-xl m-2">Top Posts</h1>
+        <PostList fetchData={fetchTopPosts} />
       </div>
 
       <div className="border px-2 py-3 space-y-2">
---
+++ src/components/posts/post-show-loading.tsx
@@ -0,0 +1,16 @@
+import { Skeleton } from '@nextui-org/skeleton';
+
+export default function PostShowLoading() {
+  return (
+    <div className="m-4">
+      <div className="my-2">
+        <Skeleton className="h-8 w-48" />
+      </div>
+      <div className="p-4 border rounded space-y-2">
+        <Skeleton className="h-5 w-32" />
+        <Skeleton className="h-5 w-32" />
+        <Skeleton className="h-5 w-32" />
+      </div>
+    </div>
+  );
+}
